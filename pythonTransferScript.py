8cdcefc13a95185a1f46e7ca1bfbf3626de48c67
hants <hantsawilliams@gmail.com>
2022-08-07 21:45:34 -0400

    python script waste of time

--- pythonTransferScript.py
+++ pythonTransferScript.py
@@ -1,48 +1,48 @@
-# run this script when you want to copy / move the files from one directory to another directory
+# # run this script when you want to copy / move the files from one directory to another directory
 
-destinationDirectory = str(input("Enter the destination directory: "))
+# destinationDirectory = str(input("Enter the destination directory (do not add a forward slash / at the end): "))
 
-# folders to move 
-import os 
-currentWD = os.getcwd()
+# # folders to move 
+# import os 
+# currentWD = os.getcwd()
 
-folders = os.listdir(currentWD)
-# remove .git folder and python transfer script 
-folders.remove('.git')
-folders.remove('pythonTransferScript.py')
+# folders = os.listdir(currentWD)
+# # remove .git folder and python transfer script 
+# folders.remove('.git')
+# folders.remove('pythonTransferScript.py')
 
-# add the currentWD to each of the files in the folders list
-for i in range(len(folders)):
-    folders[i] = currentWD + '/' + folders[i]
+# # # add the currentWD to each of the files in the folders list
+# # for i in range(len(folders)):
+# #     folders[i] = currentWD + '/' + folders[i]
 
-# based on folders, create a dictionary that determines if each is a FILE or a FOLDER
-fileDict = {}
-for folder in folders:
-    if os.path.isfile(folder):
-        fileDict[folder] = 'FILE'
-    else:
-        fileDict[folder] = 'FOLDER'
+# # based on folders, create a dictionary that determines if each is a FILE or a FOLDER
+# fileDict = {}
+# for folder in folders:
+#     if os.path.isfile(folder):
+#         fileDict[folder] = 'FILE'
+#     else:
+#         fileDict[folder] = 'FOLDER'
 
-# keep only keys that are folders in a new folder list
-folderList = []
-for key in fileDict:
-    if fileDict[key] == 'FOLDER':
-        folderList.append(key)
+# # keep only keys that are folders in a new folder list
+# folderList = []
+# for key in fileDict:
+#     if fileDict[key] == 'FOLDER':
+#         folderList.append(key)
 
-# keep only keys that are files in a new file list
-fileList = []
-for key in fileDict:
-    if fileDict[key] == 'FILE':
-        fileList.append(key)
+# # keep only keys that are files in a new file list
+# fileList = []
+# for key in fileDict:
+#     if fileDict[key] == 'FILE':
+#         fileList.append(key)
 
-# move each of the FILES in the fileList to the destination directory
-for file in fileList:
-    os.system('mv ' + file + ' ' + destinationDirectory)
-    print('moved ' + file)
+# # move each of the FILES in the fileList to the destination directory
+# for file in fileList:
+#     os.system('cp ' + file + ' ' + destinationDirectory)
+#     print('moved ' + file)
 
-# move all of the files within each FOLDER in the folderList to the destination directory
-for folder in folderList:
-    os.system('mv ' + folder + '/* ' + destinationDirectory)
-    print('moved ' + folder)
+# # move all of the files within each FOLDER in the folderList to the destination directory
+# for folder in folderList:
+#     os.system('cp -r ' + currentWD + '/' + folder + '/* ' + destinationDirectory + '/' + folder)
+#     print('moved ' + folder)
 
-print('...succesfully transfered everything to: ' + destinationDirectory)
+# print('...succesfully transfered everything to: ' + destinationDirectory)
